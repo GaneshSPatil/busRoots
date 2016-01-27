9e7d15721735e4eacbf244dfeaa342728cac23b9
ganeshpl <ganespl@INganespl.local>
2016-01-27 19:22:34 +0530

    port modified

--- server.js
+++ server.js
@@ -1,3 +1,3 @@
 var http = require('http');
 var handler = require('./lib/app.js');
-http.createServer(handler).listen(8000);
+http.createServer(handler).listen(process.env.OPENSHIFT_NODEJS_PORT || 8000, process.env.OPENSHIFT_NODEJS_IP);
